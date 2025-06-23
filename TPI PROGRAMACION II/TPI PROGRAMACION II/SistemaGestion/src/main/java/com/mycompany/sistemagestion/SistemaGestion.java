1e294aad4fbcfd29ca39ce36ea8bd796182f7280
Lisandro Boiero <110275599+Quiet2077@users.noreply.github.com>
2025-06-23 20:14:32 -0300

    Add files via upload

---
+++ TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestion;
+
+import controlador.UsuarioSistema;
+
+/**
+ *
+ * @author lichi
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        UsuarioSistema usuario = new UsuarioSistema();
+        usuario.ejMenu();
+    }
+}
