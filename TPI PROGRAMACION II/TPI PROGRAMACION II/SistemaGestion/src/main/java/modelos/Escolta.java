38cb5867308791422a08be8d822520c10cd9ec2a
Ramiro Hernandez <ramiroh.dev@gmail.com>
2025-06-26 01:17:16 -0300

    Update Escolta.java

--- TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Escolta.java
+++ TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Escolta.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelos;
 
-/**
- *
- * @author lichi
- */
+
 public class Escolta extends Jugador {
     private int tiros;
     private int manejoPelota;
