62610c150c3a1a97fcdf9b525726ed457fd0026c
Ramiro Hernandez <ramiroh.dev@gmail.com>
2025-06-26 01:17:38 -0300

    Update Jugador.java

--- TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Jugador.java
+++ TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Jugador.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelos;
 
 import java.util.Scanner;
 
-/**
- *
- * @author lichi
- */
+
 public class Jugador {
     private int id;
     private String nombreCompleto;
