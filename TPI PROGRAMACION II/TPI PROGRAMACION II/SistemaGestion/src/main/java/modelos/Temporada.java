d591ec68f5b22b71efa4510a35ac84c2a9488013
Ramiro Hernandez <ramiroh.dev@gmail.com>
2025-06-26 01:18:15 -0300

    Update Temporada.java

--- TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Temporada.java
+++ TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Temporada.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelos;
 
-/**
- *
- * @author lichi
- */
+
 public class Temporada {
     private int año;
     private String equipo;
