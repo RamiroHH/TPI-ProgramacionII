9a77321706adb0f0795c89c213e40157f1b7ca82
Ramiro Hernandez <ramiroh.dev@gmail.com>
2025-06-26 01:16:42 -0300

    Update Alero.java

--- TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Alero.java
+++ TPI PROGRAMACION II/TPI PROGRAMACION II/SistemaGestion/src/main/java/modelos/Alero.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelos;
 
-/**
- *
- * @author lichi
- */
 public class Alero extends Jugador{
     private int versatilidad;
     private int ofensivas;
